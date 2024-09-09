e7fc6f70aed2d11d0070957d1ff15f0d82a38580
blazion09 <muhd09afiq@gmail.com>
2024-09-09 14:22:38 +0800

    wip: update and remove template css

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -3,20 +3,27 @@ import { useState } from "react";
 export default function PersonalDetails() {
   const [fullName, setFullName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleFullName = (e) => setFullName(e.target.value);
   const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
 
   const details = [
     {
-      label: "Full Name:",
+      label: "Full Name: ",
       value: fullName,
       onChange: handleFullName,
     },
     {
-      label: "Phone Number:",
+      label: "Phone Number: ",
       value: phoneNumber,
       onChange: handlePhoneNumber,
+    },
+    {
+      label: "Email: ",
+      value: email,
+      onChange: handleEmail,
     },
   ];
 
