ea991b7fc8b0ff1a2027e0d001b9c6eaa9f6443a
blazion09 <muhd09afiq@gmail.com>
2024-09-09 15:26:08 +0800

    complete personal details component

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -4,10 +4,14 @@ export default function PersonalDetails() {
   const [fullName, setFullName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [currentPosition, setCurrentPosition] = useState("");
 
   const handleFullName = (e) => setFullName(e.target.value);
   const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
   const handleEmail = (e) => setEmail(e.target.value);
+  const handleAddress = (e) => setAddress(e.target.value);
+  const handleCurrentPosition = (e) => setCurrentPosition(e.target.value);
 
   const details = [
     {
@@ -24,6 +28,16 @@ export default function PersonalDetails() {
       label: "Email: ",
       value: email,
       onChange: handleEmail,
+    },
+    {
+      label: "Address: ",
+      value: address,
+      onChange: handleAddress,
+    },
+    {
+      label: "Current Position: ",
+      value: currentPosition,
+      onChange: handleCurrentPosition,
     },
   ];
 
