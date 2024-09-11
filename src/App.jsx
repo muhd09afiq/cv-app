782f3f9972efbaefe1ba6a0c166ad9af8de39a48
blazion09 <muhd09afiq@gmail.com>
2024-09-11 16:09:28 +0800

    add: form component for working experiences

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import EducationDetails from "./components/education";
 import Header from "./components/header";
 import PersonalDetails from "./components/personal";
+import ExperienceDetails from "./components/work-exp";
 
 function App() {
   return (
     <>
       <PersonalDetails />
       <EducationDetails />
+      <ExperienceDetails />
     </>
   );
 }
---
+++ src/components/work-exp.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+export default function ExperienceDetails() {
+  //(company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
+  const [companyName, setCompanyName] = useState("");
+  const [positionTitle, setPositionTitle] = useState("");
+  const [jobTask, setJobTask] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleCompanyName = (e) => setCompanyName(e.target.value);
+  const handlePositionTitle = (e) => setPositionTitle(e.target.value);
+  const handleJobTask = (e) => setJobTask(e.target.value);
+  const handleStartDate = (e) => setStartDate(e.target.value);
+  const handleEndDate = (e) => setEndDate(e.target.value);
+
+  const details = [
+    {
+      label: "Company Name: ",
+      value: companyName,
+      onChange: handleCompanyName,
+    },
+    {
+      label: "Position Name: ",
+      value: positionTitle,
+      onChange: handlePositionTitle,
+    },
+    {
+      label: "Job Task: ",
+      value: jobTask,
+      onChange: handleJobTask,
+    },
+    {
+      label: "Start Date: ",
+      value: startDate,
+      onChange: handleStartDate,
+    },
+    {
+      label: "End Date: ",
+      value: endDate,
+      onChange: handleEndDate,
+    },
+  ];
+
+  //check if there already saved data and load it
+  document.addEventListener("DOMContentLoaded", () => {
+    if (sessionStorage.getItem("ExperienceData")) {
+      const experienceData = JSON.parse(
+        sessionStorage.getItem("ExperienceData")
+      );
+      setCompanyName(experienceData[0]);
+      setPositionTitle(experienceData[1]);
+      setJobTask(experienceData[2]);
+      setStartDate(experienceData[3]);
+      setEndDate(experienceData[4]);
+    }
+  });
+
+  const formAction = function (e) {
+    e.preventDefault();
+    const experienceData = {};
+    details.forEach((item, index) => {
+      experienceData[index] = item.value;
+    });
+    sessionStorage.setItem("ExperienceData", JSON.stringify(experienceData));
+  };
+
+  return (
+    <>
+      <h3>Working Experiences</h3>
+      <form action="" onSubmit={formAction}>
+        {details.map((item, index) => (
+          <label key={index}>
+            {item.label}
+            <input value={item.value} onChange={item.onChange} />
+          </label>
+        ))}
+        <button type="submit">Save</button>
+      </form>
+    </>
+  );
+}
