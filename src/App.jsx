4410c15b2f9f6cd7b6a68cdcee30345d1ddf4435
blazion09 <muhd09afiq@gmail.com>
2024-09-12 13:46:21 +0800

    chore: separate form and resume component to different folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import EducationDetails from "./components/education";
-import Header from "./components/header";
-import PersonalDetails from "./components/personal";
-import ExperienceDetails from "./components/work-exp";
+import EducationDetails from "./components/form/education";
+import PersonalDetails from "./components/form/personal";
+import ExperienceDetails from "./components/form/work-exp";
 
 function App() {
   return (
